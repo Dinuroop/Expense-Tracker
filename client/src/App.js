f4cad8a6848f86b0143983859b3db44ac304baee
dinuroop.__286 <vijaydinuroop1983@gmail.com>
2023-03-08 21:32:42 +0530

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 
 import './App.css';
-import Form from './components/Form';
-import Graph from './components/Graph';
+import { Toaster } from 'react-hot-toast';
 import LoginPage from './pages/LoginPage';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-
-import Switcher from './Switcher';
 import Main from './pages/Main';
 import Analysis from './pages/Analysis';
 import Navbar from './components/Navbar';
@@ -20,12 +17,17 @@ function App() {
     <div class="">
       <div className="mx-auto w-[100vw] min-w-fit  bg-white min-h-[100vh] dark:bg-black text-center drop-shadow-lg text-gray-800">
       <Navbar/>
-      {/* <nav className="text-4xl py-4 mb-10 bg-slate-500 text-white rounded" style={{display:"flex",flexDirection:"row"}}>
-      <h1 className='ml-4 dark:text-black'>Expense Tracker</h1>
-      <div className='ml-auto mr-4'>
-      <Switcher/>
+      <div>
+      <Toaster
+            toastOptions={{
+              success: {
+                theme: {
+                  primary: '#4aed88',
+                        },
+                  },
+            }}
+       ></Toaster>
       </div>
-      </nav> */}
       <div className='h-full mt-8'>
       <BrowserRouter>
         <Routes>
